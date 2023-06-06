04520ed687f75ee01eb3bdf45399c24515fc95dc
JMalvin06 <73857097+JMalvin06@users.noreply.github.com>
2023-06-06 09:47:07 -0500

    big push

--- game.js
+++ game.js
@@ -20,8 +20,8 @@ let questions = [
         choice2: "system.Out.Print(\"Hello World\")",
         choice3: "printOut(\"Hello World\");",
         /* Answer */
-        choice4: "System.out.print(\"Hello World\");",
-        image: "car.png"
+        choice4: "materialism, extravagance, idea of a different life",
+        image: "shirt.png"
     },
     {
         question: "Test Question (Too much work to make a real one)",
@@ -30,10 +30,149 @@ let questions = [
         choice2: "false",
         choice3: "!true",
         /* Answer */
-        choice4: "true",
+        choice4: "Display of Gatsby\'s actuality, his \"realness\"",
+        image: "library.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "new era, isolation, absoluteness (nothing new)",
+        image: "balloon.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "display of false pretenses",
+        image: "child.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "success, wealth",
+        image: "car.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "All knowing and all seeing",
+        image: "eyes.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "death, despair, decay of morality as a result of wealth, the \"leftovers\" of this society",
         image: "ash.png"
-    }
-
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "new money, success",
+        image: "west.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "old money, power",
+        image: "east.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "smoldering remains of the lower class that is trying to rise up",
+        image: "death.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "violence,heatlessness of the society",
+        image: "gun.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "want, hope, desire, longing",
+        image: "green.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "represents his life, how much he has worked and sacrificed to impress daisy",
+        image: "house.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "Lost romance",
+        image: "daisy.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "the simultaneous desire to reach our future and relive our past",
+        image: "time.png"
+    },
+    {
+        question:"What is the correct way to print a message in java?",
+        choice1: "print(\"Hello World\")",
+        /*choice2: "System.out.print(\"Hello World\"",*/
+        choice2: "system.Out.Print(\"Hello World\")",
+        choice3: "printOut(\"Hello World\");",
+        /* Answer */
+        choice4: "Represents the mood",
+        image: "weather.png"
+    },
 ];
 
 startGame = () =>{
